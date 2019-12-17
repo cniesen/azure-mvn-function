c6f82d1d9da8eb79f37ef07ee3b96fcdebf81309
Claus <cniesen@users.noreply.github.com>
2019-12-17 11:43:26 -0600

    Made HelloWorld function distinct

--- src/main/java/com/niesens/azure/function/HelloWorld.java
+++ src/main/java/com/niesens/azure/function/HelloWorld.java
@@ -24,9 +24,9 @@ public class HelloWorld {
         String name = request.getBody().orElse(query);
 
         if (name == null) {
-            return request.createResponseBuilder(HttpStatus.BAD_REQUEST).body("Please pass a name on the query string or in the request body").build();
+            return request.createResponseBuilder(HttpStatus.BAD_REQUEST).body("(hello-world) Please pass a name on the query string or in the request body").build();
         } else {
-            return request.createResponseBuilder(HttpStatus.OK).body("Hello, " + name).build();
+            return request.createResponseBuilder(HttpStatus.OK).body("(hello-world) Hello, " + name).build();
         }
     }
 }
